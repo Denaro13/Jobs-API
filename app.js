05849d5bb1a550c27545a27b3e702b5612f94515
Usman Nurudeen <usmannurudeen13@gmail.com>
2024-03-01 03:00:14 +0100

    New commit

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cors());
 app.use(xss());
 
 // routes
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("<h1>Jobs API</h1>");
 });
 app.use("/api/v1/auth", authRouter);
