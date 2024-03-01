2e55409238ef417b979c1d22711067b041e61ae0
Usman Nurudeen <usmannurudeen13@gmail.com>
2024-03-01 05:48:27 +0100

    Swagger Documentation Added

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 require("express-async-errors");
 
-const express = require("express");
-const app = express();
-
 //extra securities
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
+
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocs = YAML.load("./swagger.yaml");
+
+const express = require("express");
+const app = express();
 
 // connectDB
 const connectDB = require("./db/connect");
@@ -36,10 +41,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// routes
 app.get("/", (req, res) => {
-  res.send("<h1>Jobs API</h1>");
+  res.send("<h1>Jobs API</h1><a href='/api-docs' >Documentation</a>");
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+// routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
