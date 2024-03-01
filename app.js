f5fb27111ced747352d3423beba8fca7829ee61b
Usman Nurudeen <usmannurudeen13@gmail.com>
2024-03-01 02:50:07 +0100

    Fixed home route

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.use("/", (req, res) => {
+  res.send("<h1>Jobs API</h1>");
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
